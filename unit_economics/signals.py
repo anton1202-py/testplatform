6787faf973476a7b1373f6434a146d7e2ec838f0
e.v.solovey <e.v.solovey@inbox.ru>
2024-09-11 18:23:44 +0300

    обновления для тестов

--- unit_economics/signals.py
+++ unit_economics/signals.py
@@ -2,15 +2,113 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from unit_economics.models import MarketplaceCommission, MarketplaceLogistic
+from unit_economics.integrations import (changer_price_with_profitability,
+                                         changer_profitability_calculate)
+from unit_economics.models import (MarketplaceCommission, MarketplaceLogistic,
+                                   MarketplaceProduct,
+                                   MarketplaceProductPriceWithProfitability,
+                                   ProductCostPrice,
+                                   ProductForMarketplacePrice,
+                                   ProductOzonPrice,
+                                   ProfitabilityMarketplaceProduct)
 
 
-# Шаг 3: Создайте обработчик сигнала
+@receiver(post_save, sender=MarketplaceLogistic)
+def profitability_logistic_update(sender, instance, created, **kwargs):
+    """
+    Пересчет рентабельности при изменении стоимости логистики
+    """
+    product = instance.marketplace_product
+    try:
+        changer_profitability_calculate(product)
+    except Exception as e:
+        message = f'Не удалось обновить рентабельность для продукта: {product}. Ошибка: {e}'
+        print(message)
+
+
 @receiver(post_save, sender=MarketplaceCommission)
-def logistic_update(sender, instance, created, **kwargs):
-    if created:
-        pass
-        # print(f'Создана новая запись: {instance.marketplace_product}')
-    else:
-        pass
-        # print(f'Обновлена запись: {instance.marketplace_product}')
+def profitability_comission_update(sender, instance, created, **kwargs):
+    """
+    Пересчет рентабельности при изменении стоимости комиссии
+    """
+    product = instance.marketplace_product
+    print('В сигнале комиссии', {product})
+    try:
+        print('Вызываю функцию сохранения', {product})
+        changer_profitability_calculate(product)
+        print('Прошел функцию сохранения', {product})
+    except Exception as e:
+        message = f'Не удалось обновить рентабельность для продукта: {product}. Ошибка: {e}'
+        print(message)
+
+
+@receiver(post_save, sender=ProductForMarketplacePrice)
+def profitability_price_wb_ya_update(sender, instance, created, **kwargs):
+    """
+    Пересчет рентабельности при изменении цены на Яндексе и Вб
+    """
+    main_product = instance.product
+    products_list = MarketplaceProduct.objects.filter(product=main_product)
+    for product in products_list:
+        try:
+            changer_profitability_calculate(product)
+        except Exception as e:
+            message = f'Не удалось обновить рентабельность для продукта: {product}. Ошибка: {e}'
+            print(message)
+
+
+@receiver(post_save, sender=ProductOzonPrice)
+def profitability_price_oz_update(sender, instance, created, **kwargs):
+    """
+    Пересчет рентабельности при изменении цены на Озоне
+    """
+    main_product = instance.product
+    products_list = MarketplaceProduct.objects.filter(product=main_product)
+    for product in products_list:
+        try:
+            changer_profitability_calculate(product)
+        except Exception as e:
+            message = f'Не удалось обновить рентабельность для продукта: {product}. Ошибка: {e}'
+            print(message)
+
+
+# @receiver(post_save, sender=ProfitabilityMarketplaceProduct)
+# def profitability_price_oz_update(sender, instance, created, **kwargs):
+#     """
+#     Пересчет рентабельности при изменении Накладных расходов
+#     """
+#     product = instance.mp_product
+#     try:
+#         changer_profitability_calculate(product)
+#     except Exception as e:
+#         message = f'Не удалось обновить рентабельность для продукта: {product}. Ошибка: {e}'
+#         print(message)
+
+
+@receiver(post_save, sender=ProfitabilityMarketplaceProduct)
+def price_profitability_price_oz_update(sender, instance, created, **kwargs):
+    """
+    Пересчет цены на основе рентабельности при изменении Накладных расходов
+    """
+    product = instance.mp_product
+    try:
+        changer_price_with_profitability(product)
+    except Exception as e:
+        message = f'Не удалось обновить цену на основе рентабельности для продукта: {product}. Ошибка: {e}'
+        print(message)
+
+
+@receiver(post_save, sender=ProductCostPrice)
+def profitability_price_oz_update(sender, instance, created, **kwargs):
+    """
+    Пересчет цены на основе рентабельности при изменении
+    себестоимости Продукта по методу оприходования по FIFO
+    """
+    main_product = instance.product
+    products_list = MarketplaceProduct.objects.filter(product=main_product)
+    for product in products_list:
+        try:
+            changer_price_with_profitability(product)
+        except Exception as e:
+            message = f'Не удалось обновить цену на основе рентабельности для продукта: {product}. Ошибка: {e}'
+            print(message)
