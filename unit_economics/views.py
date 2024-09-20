df93e4791af7cb6f54a8614a8f758a465e50fd53
anton1202-py <anton1202@yandex.ru>
2024-09-20 17:35:15 +0300

    Ошибка с price

--- unit_economics/views.py
+++ unit_economics/views.py
@@ -369,7 +369,7 @@ class MarketplaceProductViewSet(viewsets.ReadOnlyModelViewSet):
             serializer = self.get_serializer(page, many=True)
             print('Я после суриализатор')
             
-            x = serializer.data[11:100]
+            x = serializer.data[101:400]
             for i in x:
                 print('**************************')
                 print(i)
